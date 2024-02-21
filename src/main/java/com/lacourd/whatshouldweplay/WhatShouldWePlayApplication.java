31ac2dce8a49282727a6ce6d05596aab450f1817
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2024-02-21 12:43:16 -0600

    Initial commit

---
+++ src/main/java/com/lacourd/whatshouldweplay/WhatShouldWePlayApplication.java
@@ -0,0 +1,13 @@
+package com.lacourd.whatshouldweplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldWePlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatShouldWePlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lacourd/whatshouldweplay/WhatShouldWePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacourd.whatshouldweplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatShouldWePlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
