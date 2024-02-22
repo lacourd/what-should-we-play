c0ce7fd4c0912cb14fd012e731ffa8dab3494bb6
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2024-02-22 14:20:48 -0600

    More work from the Spring Security tutorial video

--- src/main/java/com/lacourd/whatshouldweplay/config/SecurityConfig.java
+++ src/main/java/com/lacourd/whatshouldweplay/config/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.lacourd.whatshouldweplay.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
 }
